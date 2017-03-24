fcbdd450d1c1eedbfac3aa07702423c7b21ebaf1
Petr Karmashev <smonkl@bk.ru>
2017-03-25 01:30:40 +0300

    Dynamical grid rendering preparations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = 'Hexammon';
+    title: string = 'Hexammon';
+    rowsNumber: number = 6;
+
+    getNumberOfRowsPairAsArray(): Array<number> {
+        return new Array(this.rowsNumber);
+    }
 }
