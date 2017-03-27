22fb7678eaaa3a243abaa9e8152c0c8f7a48197f
Petr Karmashev <smonkl@bk.ru>
2017-03-28 02:14:35 +0300

    Stub rooms generation

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -4,7 +4,6 @@ import { GridComponent } from "./components/grid/grid.component";
 import { ThresholdComponent } from "./components/thrashold/threshold.component";
 
 const routes: Routes = [
-    { path: '', redirectTo: '/threshold', pathMatch: 'full' },
     { path: 'threshold', component: ThresholdComponent },
     { path: 'game', component: GridComponent },
 ];
